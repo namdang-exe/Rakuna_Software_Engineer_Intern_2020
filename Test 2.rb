a916941f609583c3d2cb1ed4a0ca1a1bce299e80
Nam Dang <dangquangnam75@gmail.com>
2020-07-20 18:28:36 -0500

    Add files via upload

--- Test 2.rb
+++ Test 2.rb
@@ -1,3 +1,9 @@
+"""
+
+The code was very badly documented, so I changed the name to make it easier to debug
+Also, the logic at the end was wrong. The boundary for the clone was 0 < clone < width, rather than finding where the portal is, and moving accordingly
+
+"""
 STDOUT.sync = true
 _,width,_,exit_floor,exit_pos,_,_,nb_elevators=gets.split.map(&:to_i)
 
@@ -10,5 +16,7 @@ loop{
 clone_floor,clone_pos,direction=gets.split;
 clone_floor,clone_pos=clone_floor.to_i,clone_pos.to_i; 
 elevator_pos = clone_floor==exit_floor ? exit_pos : matrix[clone_floor];
+
+# This was wrong
 puts (((clone_pos < elevator_pos.to_i) & (direction != "RIGHT")) || ((clone_pos > elevator_pos.to_i) & (direction != "LEFT"))) ? "BLOCK" : "WAIT"
 }
